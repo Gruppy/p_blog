589de1ffaccc4b1038cc4462d3a5c252a9eb11fd
yossan4343434 <yossan4343434@gmail.com>
2015-07-16 09:51:20 +0900

    Change database from sqlite to mysql

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150716003536) do
+
+  create_table "params_sets", force: :cascade do |t|
+    t.string   "param_a",    limit: 255
+    t.string   "float",      limit: 255
+    t.string   "param_b",    limit: 255
+    t.string   "param_c",    limit: 255
+    t.string   "param_d",    limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
 end
